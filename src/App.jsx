121d69b5d20620041a918dee95910c99be1179fd
Mauricio Mancilla <mauriciomancilla@Mauricios-MacBook-Pro.local>
2023-07-17 10:47:16 -0400

    primera version

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import Calculator from './Calculator.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mi App</h1>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
+
